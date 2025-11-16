00b4f2edb17fa4db009146978bd3aeb28f06c2be
MananBagadi100 <mananbagadi100@gmail.com>
2025-11-16 22:03:27 +0530

    cors (debugging)

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,8 +32,8 @@ async function loginAdmin(req, res) {
         // Send JWT in cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,           // true in production (HTTPS)
-            sameSite: "lax",
+            secure: true,           // true in production (HTTPS)
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
